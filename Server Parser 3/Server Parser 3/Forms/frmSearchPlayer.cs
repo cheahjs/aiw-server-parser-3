e0f667604991e5a205a139eff5c21f6fb5ec5ab0
cheahjunsiang@gmail.com <cheahjunsiang@gmail.com@adc4ad3d-a3db-31a2-f21b-f376215f20f6>
2011-03-13 07:12:26 +0000

    Update to v5 Simple RCON tool has been implemented NTAuthority's Log class has been implemented, uncomment Log.Initialize to enable it (warning, huge logs are generated)

---
+++ Server Parser 3/Server Parser 3/Forms/frmSearchPlayer.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Intelectix.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Server_Parser_3
+{
+    public partial class frmSearchPlayer : Form
+    {
+        #region Variables
+        List<Server> _search = new List<Server>();
+        private ListViewSortManager m_sortMgr;
+        #endregion
+
+        #region Form Methods
+        public frmSearchPlayer()
+        {
+            InitializeComponent();
+            m_sortMgr = new ListViewSortManager(listViewFound, new Type[] {
+                typeof(ListViewTextCaseInsensitiveSort),
+                typeof(ListViewTextCaseInsensitiveSort),
+                typeof(ListViewIPSort) });
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            findPlayer(txtSearch.Text);
+        }
+        private void findPlayer(string name)
+        {
+            var foundplayers = new Dictionary<string, Server>();
+            var servers = _search;
+            var randomizer = new Random(Environment.TickCount);
+            foreach (var server in servers)
+            {
+                foreach (var player in server.Players)
+                {
+                    if (!string.IsNullOrEmpty(player.Name) && player.Name != "\0")
+                    {
+                        if (player.Name.ToUpper().IndexOf(name.ToUpper()) != -1 || removeQuakeColorCodes(player.Name).ToUpper().IndexOf(name.ToUpper()) != -1)
+                        {
+                            var playername = player.Name + randomizer.Next(1000000, 9999999);
+                            foundplayers.Add(playername, server);
+                        }
+                    }
+                }
+            }
+            displayPlayers(foundplayers);
+        }
+        private void displayPlayers(Dictionary<string, Server> players)
+        {
+            listViewFound.Items.Clear();
+            m_sortMgr.SortEnabled = false;
+            var length = 0;
+            foreach (var item in players)
+            {
+                try
+                {
+                    var listitem = listViewFound.Items.Add(removeQuakeColorCodes(item.Key));//(item.Key.Substring(0, item.Key.Length - 7));
+                    listitem.SubItems.Add(removeQuakeColorCodes(getValue(item.Value.Dvars, "sv_hostname")));
+                    listitem.SubItems.Add(item.Value.IP + ":" + item.Value.Port.ToString());
+                    length = (removeQuakeColorCodes(item.Key).Length - 7);
+                    listitem.Text = item.Key.Substring(0, length);
+                }
+                catch { }
+            }
+            m_sortMgr.SortEnabled = true;
+            listViewFound.Sort();
+        }
+        private void frmSearchPlayer_Load(object sender, EventArgs e)
+        {
+            _search = Form1._searchServers;
+        }
+        private void listViewFound_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.C && e.Control)
+            {
+                var selectedItem = listViewFound.SelectedItems[0];
+                if (selectedItem != null)
+                {
+                    Clipboard.SetDataObject(selectedItem.Text + " - " + selectedItem.SubItems[1].Text + " - " + selectedItem.SubItems[2].Text);
+                    e.Handled = true;
+                }
+            }
+        }
+        private void listViewFound_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (listViewFound.SelectedItems.Count > 0)
+            {
+                connectIP(listViewFound.SelectedItems[0].SubItems[2].Text);
+            }
+        }
+        #endregion
+
+        #region Misc Methods
+        private string getValue(Dictionary<string, string> dict, string key)
+        {
+            try
+            {
+                return dict[key];
+            }
+            catch
+            {
+                return "";
+            }
+        }
+        private string removeQuakeColorCodes(string remove)
+        {
+            string filteredout = "";
+            var array = remove.Split('^');
+            if (array.Length == 1)
+                return remove;
+            foreach (string part in array)
+            {
+                if (part.StartsWith("0") || part.StartsWith("1") || part.StartsWith("2") || part.StartsWith("3") || part.StartsWith("4") || part.StartsWith("5") || part.StartsWith("6") || part.StartsWith("7") || part.StartsWith("8") || part.StartsWith("9"))
+                    filteredout += part.Substring(1);
+                else
+                    filteredout += "^" + part;
+            }
+            return filteredout.Substring(1);
+        }
+        private void connectIP(object IP)
+        {
+            var ip = (string)IP;
+            if (Process.GetProcessesByName("iw4mp.dat").Count() == 0)
+            {
+                Process.Start("aiw://connect/deathmax'sserverparser:65540");
+                Thread.Sleep(35);
+            }
+            Process.Start("aiw://connect/" + ip);
+        }
+        #endregion
+    }
+}
