7af8fb5aa7b6f8677963bff0b550ec17a6701613
cheahjunsiang <cheahjunsiang@adc4ad3d-a3db-31a2-f21b-f376215f20f6>
2012-04-21 09:12:11 +0000

    Update to r9 with non-working async code

---
+++ Server Parser 3/Server Parser 3/Classes/ListViewExtensions.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Server_Parser_3.Classes
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class ListViewExtensions
+    {
+        private const Int32 HDI_FORMAT = 0x4;
+        private const Int32 HDF_SORTUP = 0x400;
+        private const Int32 HDF_SORTDOWN = 0x200;
+        private const Int32 LVM_GETHEADER = 0x101f;
+        private const Int32 HDM_GETITEM = 0x120b;
+        private const Int32 HDM_SETITEM = 0x120c;
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", EntryPoint = "SendMessage")]
+        private static extern IntPtr SendMessageLVCOLUMN(IntPtr hWnd, Int32 Msg, IntPtr wParam, ref LVCOLUMN lPLVCOLUMN);
+
+        public static void SetSortIcon(this ListView ListViewControl, int ColumnIndex, SortOrder Order)
+        {
+            IntPtr ColumnHeader = SendMessage(ListViewControl.Handle, LVM_GETHEADER, IntPtr.Zero, IntPtr.Zero);
+
+            for (int ColumnNumber = 0; ColumnNumber <= ListViewControl.Columns.Count - 1; ColumnNumber++)
+            {
+                var ColumnPtr = new IntPtr(ColumnNumber);
+                var lvColumn = new LVCOLUMN();
+                lvColumn.mask = HDI_FORMAT;
+                SendMessageLVCOLUMN(ColumnHeader, HDM_GETITEM, ColumnPtr, ref lvColumn);
+
+                if (!(Order == SortOrder.None) && ColumnNumber == ColumnIndex)
+                {
+                    switch (Order)
+                    {
+                        case SortOrder.Ascending:
+                            lvColumn.fmt &= ~HDF_SORTDOWN;
+                            lvColumn.fmt |= HDF_SORTUP;
+                            break;
+                        case SortOrder.Descending:
+                            lvColumn.fmt &= ~HDF_SORTUP;
+                            lvColumn.fmt |= HDF_SORTDOWN;
+                            break;
+                    }
+                }
+                else
+                {
+                    lvColumn.fmt &= ~HDF_SORTDOWN & ~HDF_SORTUP;
+                }
+
+                SendMessageLVCOLUMN(ColumnHeader, HDM_SETITEM, ColumnPtr, ref lvColumn);
+            }
+        }
+
+        #region Nested type: LVCOLUMN
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct LVCOLUMN
+        {
+            public Int32 mask;
+            public readonly Int32 cx;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public readonly string pszText;
+            public readonly IntPtr hbm;
+            public readonly Int32 cchTextMax;
+            public Int32 fmt;
+            public readonly Int32 iSubItem;
+            public readonly Int32 iImage;
+            public readonly Int32 iOrder;
+        }
+
+        #endregion
+    }
+
+    internal class ListViewNF : ListView
+    {
+        public ListViewNF()
+        {
+            //Activate double buffering
+            SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint, true);
+
+            //Enable the OnNotifyMessage event so we get a chance to filter out 
+            // Windows messages before they get to the form's WndProc
+            SetStyle(ControlStyles.EnableNotifyMessage, true);
+        }
+
+        protected override void OnNotifyMessage(Message m)
+        {
+            //Filter out the WM_ERASEBKGND message
+            if (m.Msg != 0x14)
+            {
+                base.OnNotifyMessage(m);
+            }
+        }
+    }
+}
